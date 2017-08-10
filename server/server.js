11c65bad8a6643f6d053ab6348c9fff1c3858114
Bishop Zareh <xchicago@gmail.com>
2017-08-10 10:16:41 -0500

    update the node server code

--- server/server.js
+++ server/server.js
@@ -2,21 +2,35 @@
 'use strict';
 
 const express = require('express');
-const app = express();
-// const livereload = require('livereload');
+const http = require('http');
+
+const server = express();
 
 var serverPort = process.env.PORT || 8080;
 
-if (app.get('env') !== 'production') {
-  app.use(require('connect-livereload')({
-    port: 35729
-  }));
+// Development Server Options
+
+const isDev = process.env.NODE_ENV !== 'production';
+const env  = isDev ? 'development' : process.env.NODE_ENV;
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || 'localhost';
+
+server.set('env', env);
+server.set('host', host); 
+server.set('port', port);
+
+if (isDev) {
+  server.use(require('connect-livereload')({ port: 35729 }));
 }
 
-app.use(express.static('./distribution'));
+// Production Server Options
 
-/* const server = */ app.listen(serverPort, () => {
-  console.log('=================================');
-  console.log('  Server listening on port ' + serverPort);
-  console.log('=================================');
-});
+// static paths for the rest
+server.use(express.static('./distribution'));
+
+// Create the server
+http.createServer(server).listen(port);
+
+console.log('=================================');
+console.log('  Server listening on port ' + port);
+console.log('=================================');
